8bea6b06492bf97e3b82510604d731f41da3055b
Nikola Gazdic <nikola.gazdic90@gmail.com>
2022-07-01 12:01:11 -0400

    made changes to the project

---
+++ Variables/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string myName = "Nikola";
+int myAge = 31;
+char firstLetter = 'N';
+bool cool = true;
+double randomDouble = 21.5;
+decimal randomDecimal = 31.29807m;
+
+Console.WriteLine($"Hello {myName},pretty old huh {myAge}! Anyways, you are cool {cool}, and first letter of your name is {firstLetter}. Two random numbers {randomDouble} & {randomDecimal}");
+
+
+
+
