963015d58a57bb58a7255b1e9a9110938eaf5dff
Nikola Gazdic <nikola.gazdic90@gmail.com>
2022-07-05 16:54:15 -0400

    last commit

---
+++ Variables/Hello.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Variables
+{
+    public class TheNames
+    {
+        public static void Hello() {
+
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("What is your age?");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What is your favorite animal?");
+            string animal = Console.ReadLine();
+
+            Console.WriteLine($"My name is {name} and I am {num} years old. My favorite animal is a {animal}.");
+
+
+        }
+
+       
+    }
+}
+
--- Variables/Program.cs
+++ Variables/Program.cs
@@ -1,14 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Variables;
 
-string myName = "Nikola";
-int myAge = 31;
-char firstLetter = 'N';
-bool cool = true;
-double randomDouble = 21.5;
-decimal randomDecimal = 31.29807m;
 
-Console.WriteLine($"Hello {myName},pretty old huh {myAge}! Anyways, you are cool {cool}, and first letter of your name is {firstLetter}. Two random numbers {randomDouble} & {randomDecimal}");
+
+TheNames.Hello();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//string myName = "Nikola";
+//int myAge = 31;
+//char firstLetter = 'N';
+//bool cool = true;
+//double randomDouble = 21.5;
+//decimal randomDecimal = 31.29807m;
+
+//Console.WriteLine($"Hello {myName},pretty old huh {myAge}! Anyways, you are cool {cool}, and first letter of your name is {firstLetter}. Two random numbers {randomDouble} & {randomDecimal}");
 
 
 
